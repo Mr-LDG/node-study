d6b214a7d12ebdd84e52d2c40d1a9858ee8bf0fe
dglee <dglee@amuzlab.com>
2023-05-24 11:48:50 +0900

    뱀스 요청 서버 만들기 위한 초기상태

--- os.js
+++
@@ -1,4 +0,0 @@
-const os = require('os');
-
-let plat = os.platform();
-console.log("this os is ", plat);
--- sort.js
+++
@@ -1,6 +0,0 @@
-var a = [3,1,2];
-function b(v1,v2){
-    return v2-v1;
-};
-a.sort(b);
-console.log(a);
